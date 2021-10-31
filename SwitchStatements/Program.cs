1e5fbc0763aae4a52d8fb1a9d674bd3f650e8f89
Frederick Bradley <fbradley21@icloud.com>
2021-10-31 15:48:45 -0500

    completed switch statements exercise

---
+++ SwitchStatements/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SwitchStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi! What is your favorite school subject?");
+
+            var subject = Console.ReadLine();
+
+            switch (subject.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("Math is a tough subject!");
+                    break;
+                case "science":
+                    Console.WriteLine("Science is a tough subject!");
+                    break;
+                case "english":
+                    Console.WriteLine("English is an interesting subject!");
+                    break;
+                default:
+                    Console.WriteLine($"Oh wow! I haven't taken that subject before. {subject} sounds fun!");
+                    break;
+
+            }
+        }
+    }
+}
